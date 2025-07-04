d51033bdd0c35e56284cd3c8d678ebebdc098d0f
Gabriel Vieira <gabrielvieira1@protonmail.com>
2025-07-04 15:53:45 -0300

    Fix error env

--- database/db.go
+++ database/db.go
@@ -14,12 +14,22 @@ var (
 )
 
 func ConectaComBancoDeDados() {
+	host := os.Getenv("HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+	port := os.Getenv("PORT")
 	
-	stringDeConexao := "host="+os.Getenv("HOST")+" user="+os.Getenv("DB_USER")+" password="+os.Getenv("DB_PASSWORD")+" dbname="+os.Getenv("DB_NAME")+" port="+os.Getenv("PORT")+" sslmode=disable"
+	// Debug (sem mostrar senha)
+	log.Printf("Conectando ao banco: host=%s user=%s dbname=%s port=%s", host, user, dbname, port)
+	
+	stringDeConexao := "host="+host+" user="+user+" password="+password+" dbname="+dbname+" port="+port+" sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
+		log.Printf("Erro na conexão: %v", err)
 		log.Panic("Erro ao conectar com banco de dados")
 	}
 
+	log.Println("Conexão com banco estabelecida com sucesso!")
 	_ = DB.AutoMigrate(&models.Aluno{})
 }
